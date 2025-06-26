d4d74d72178ed68a82ee94210a8fc0cff2e4a27a
Yashwanth Krishna Devendran <yashwanth23110541@gmail.com>
2025-06-26 14:40:06 +0000

    Evaluation using Linear Models

--- HousePricePredictionDataset/app.py
+++ HousePricePredictionDataset/app.py
@@ -1,11 +1,11 @@
 # Task 3 : 26/June/2025
 
 # Importing all necessary preprocessing libraries
-import numpy as np
+# import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.impute import SimpleImputer
+# from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OrdinalEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
@@ -13,6 +13,7 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 #import required models
 from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.svm import SVR
 
 # Loading the dataset
 data_set = pd.read_csv("HousePricePredictionDataset/Housing.csv")
@@ -34,22 +35,22 @@ enc = OrdinalEncoder()
 data[categorical_cols] = enc.fit_transform(data[categorical_cols])
 
 # visulaise the data
-# for col in numerical_cols :
-#   plt.figure(figsize=(6,4))
-#   sns.boxplot(x= data[col], color='skyblue')
-#   plt.title(f'Box plot visulization of {col}')
-#   plt.xlabel(col)
-#   plt.tight_layout()
-#   plt.show()
+for col in numerical_cols :
+  plt.figure(figsize=(6,4))
+  sns.boxplot(x= data[col], color='skyblue')
+  plt.title(f'Box plot visulization of {col}')
+  plt.xlabel(col)
+  plt.tight_layout()
+  plt.show()
 
-# for col in numerical_cols:
-#   plt.figure(figsize=(6,4))
-#   sns.histplot(x= data[col], bins=30, kde=True, color='orange')
-#   plt.title(f'Box plot visulization of {col}')
-#   plt.xlabel(col)
-#   plt.ylabel("frequency")
-#   plt.tight_layout()
-#   plt.show()
+for col in numerical_cols:
+  plt.figure(figsize=(6,4))
+  sns.histplot(x= data[col], bins=30, kde=True, color='orange')
+  plt.title(f'Box plot visulization of {col}')
+  plt.xlabel(col)
+  plt.ylabel("frequency")
+  plt.tight_layout()
+  plt.show()
 
 # scaling the features
 scaler = StandardScaler()
@@ -70,6 +71,8 @@ predicted_price = model.predict(X_test)
 mae = mean_absolute_error(y_test, predicted_price)
 mse = mean_squared_error(y_test, predicted_price)
 R_sqaure_value = r2_score(y_test, predicted_price)
+
+print("\nLinear Regression Model Evaluation\n")
 print(f"The mean absolute error in prediction = {mae}")
 print(f"The mean squared error in prediction = {mse}")
 print(f"The R square value of prediction = {R_sqaure_value}")
@@ -78,8 +81,34 @@ print(f"The R square value of prediction = {R_sqaure_value}")
 plt.figure(figsize=(6, 6))
 sns.scatterplot(x=y_test, y=predicted_price, color='teal')
 plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--')  # Ideal line
+plt.title("Linear Regression Prediction")
 plt.xlabel("Actual Prices")
 plt.ylabel("Predicted Prices")
-plt.title("Actual vs Predicted House Prices")
 plt.tight_layout()
 plt.show()
+
+# Logistic Regression
+model = LogisticRegression()
+model.fit(X_train, y_train)
+predicted_price = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, predicted_price)
+mse = mean_squared_error(y_test, predicted_price)
+R_sqaure_value = r2_score(y_test, predicted_price)
+print("\nLogistic Regression Model Evaluation\n")
+print(f"The mean absolute error in prediction = {mae}")
+print(f"The mean squared error in prediction = {mse}")
+print(f"The R square value of prediction = {R_sqaure_value}")
+
+# Visualise prediction accuracy 
+plt.figure(figsize=(6, 6))
+sns.scatterplot(x=y_test, y=predicted_price, color='black')
+plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--')  # Ideal line
+plt.title("Logistic Regression Prediction")
+plt.xlabel("Actual Prices")
+plt.ylabel("Predicted Prices")
+plt.tight_layout()
+plt.show()
+
+
+
